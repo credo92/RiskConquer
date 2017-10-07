8a42ae2b91568f76cdacaed5eba24ab73c188e93
Vipul Srivastav <vipulsrivastav92@gmail.com>
2017-10-07 17:46:24 -0400

    Turn Initial Draft

--- src/main/java/com/app/team19/gameplay/GameplayController.java
+++ src/main/java/com/app/team19/gameplay/GameplayController.java
@@ -1,12 +1,35 @@
 package com.app.team19.gameplay;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GameplayController {
 
     private int number;
-
+    /**
+     * players object holds the players information
+     * @author vipulsrivastav
+     */
+//    private ArrayList<Player> players;
+    
+    
     public int selectedNumberOfPlayers(int number){
         this.number = number;
         System.out.println(this.number);
         return this.number;
     }
+    /**Turn Order
+     * @author vipulsrivastav
+     * @param numberOfPlayers
+     * @param players 
+     */
+    public void turnOrder(int numberOfPlayers, List<?> players) {
+    	// Update according to Player.java class
+    	Collections.shuffle(players);
+    	//nextPlayer();
+    }
+    
+    
+    
 }
