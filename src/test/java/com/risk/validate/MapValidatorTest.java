77289676310b85dbc8f2987e5b56686fe00f6abd
Garvpreet Singh <garvs1401@gmail.com>
2017-10-18 16:58:16 -0400

    added more test cases and changed code in MapValidator.java

---
+++ src/test/java/com/risk/validate/MapValidatorTest.java
@@ -0,0 +1,109 @@
+package com.risk.validate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.risk.entity.Continent;
+import com.risk.entity.Map;
+import com.risk.entity.Territory;
+import com.risk.exception.InvalidMapException;
+
+/**
+ * Map Validator Test class.
+ * @author Garvpreet Singh
+ * @version 1.0.0
+ */
+public class MapValidatorTest {
+	
+	static MapValidator mapValidator;
+	
+	static Continent continent;
+	static Territory territory;
+	static Map map;
+	
+	String mapAuthor = "Robert";
+	String mapImage = "world.map";
+	String mapWrap = "no";
+	String mapScroll = "horizontal";
+	String mapWarn = "yes";
+	
+	String continentName = "Asia";
+	String controlValue = "7";
+	
+	static HashMap<String, String> mapData;
+	List<Continent> listOfContinents;
+	
+	/**
+	 * This method is invoked at the start of the test class.
+	 */
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Testing MapValidator.java started");
+		continent = new Continent();
+		territory = new Territory();
+		map = new Map();
+		mapValidator = new MapValidator();
+	}	
+	
+	/**
+	 * This method is invoked at the start of all the test methods.
+	 */
+	@Before
+	public void beforeTest() {
+		mapData = new HashMap<>();
+		mapData.put("author", mapAuthor);
+		mapData.put("image", mapImage);
+		mapData.put("wrap", mapWrap);
+		mapData.put("scroll", mapScroll);
+		mapData.put("warn", mapWarn);		
+		map.setMapData(mapData);
+		
+		continent.setName(continentName);
+		continent.setValue(controlValue);
+		
+		listOfContinents = new ArrayList<>();
+		listOfContinents.add(continent);
+	}
+	
+	/**
+	 * This method is invoked at the end of the test class.
+	 */
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("Testing MapValidator.java completed");
+	}
+	
+	/**
+	 * This method is used to test if a map is null or not.
+	 * @throws InvalidMapException invalid map exception.
+	 */
+	@Test (expected = InvalidMapException.class)
+	public void validateMapForNullMap() throws InvalidMapException {
+		MapValidator.validateMap(null);
+	}
+	
+	/**
+	 * This method is used to test if a map has atleast one continent or not.
+	 * @throws InvalidMapException invalid map exception.
+	 */
+	@Test (expected = InvalidMapException.class)
+	public void validateMapForContinent() throws InvalidMapException {
+		MapValidator.validateMap(new Map());
+	}
+	
+	/**
+	 * This method is used to test if a continent has atleast one territory or not.
+	 * @throws InvalidMapException invalid map exception.
+	 */
+	@Test (expected = InvalidMapException.class)
+	public void validateContinentForTerritory() throws InvalidMapException {
+		map.setContinents(listOfContinents);
+		MapValidator.validateContinent(continent,map);	
+	}
+}
