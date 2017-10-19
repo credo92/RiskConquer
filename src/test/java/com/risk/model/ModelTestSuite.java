6cb9bf3841d33ab0512a7f73c18f89500f049349
Garvpreet Singh <garvs1401@gmail.com>
2017-10-19 13:34:51 -0400

    added two test suites

---
+++ src/test/java/com/risk/model/ModelTestSuite.java
@@ -0,0 +1,10 @@
+package com.risk.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({GameModelTest.class, MapModelTest.class})
+public class ModelTestSuite {
+}
---
+++ src/test/java/com/risk/validate/ValidateTestSuite.java
@@ -0,0 +1,10 @@
+package com.risk.validate;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({MapValidatorTest.class})
+public class ValidateTestSuite {
+}
