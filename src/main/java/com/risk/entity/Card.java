9021a2d2fc81a569e30be8d471d3ad5b468ef689
Vipul Srivastav <vipulsrivastav92@gmail.com>
2017-10-27 00:17:59 -0400

    javadocs added for Card.java class

--- src/main/java/com/risk/entity/Card.java
+++ src/main/java/com/risk/entity/Card.java
@@ -4,7 +4,7 @@ import com.risk.constant.CardType;
 
 /**
  * @author vipul
- * This is an entity class for Card with cardEnum and Territory object
+ * This is an entity class for Card with cardType object and territoryName as its member variable
  * @version 1.0.1
  * 
  */
@@ -13,15 +13,26 @@ public class Card {
 	CardType cardType;
 	private String territoryName;
 	
-	public String getTerritoryName() {
-		return territoryName;
-	}
-	public void setTerritoryName(String territoryName) {
-		this.territoryName = territoryName;
-	}
 	public Card(CardType cardType){
 		this.cardType = cardType;
 	}
+	
+	/**
+	 * @return the territoryName
+	 */
+	public String getTerritoryName() {
+		return territoryName;
+	}
+	/**
+	 * @param territoryName
+	 *            the territoryName to set
+	 */
+	public void setTerritoryName(String territoryName) {
+		this.territoryName = territoryName;
+	}
+	/**
+	 * @return the cardType
+	 */
 	public CardType getCardType() {
 		return cardType;
 	}
