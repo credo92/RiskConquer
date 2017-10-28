52ddee265695a8a7d1e8d16d19db205f43ae99f4
gurpreetsingh2410 <gurpreetsingh2410@gmail.com>
2017-10-28 14:55:39 -0400

    layout name changed

---
+++ src/main/java/com/risk/main/Dice.java
@@ -0,0 +1,16 @@
+package com.risk.main;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+public class Dice implements EventHandler<ActionEvent> {
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		final Stage diceStage = new Stage();
+		diceStage.setTitle("Roll Dice");
+	}
+
+}
