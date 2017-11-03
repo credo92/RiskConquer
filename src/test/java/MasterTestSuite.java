64fee54a3f86412a97d36acf2a734b7192c2c36f
Garvpreet Singh <garvs1401@gmail.com>
2017-11-03 12:01:04 -0400

    added master test suite file

---
+++ src/test/java/MasterTestSuite.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.risk.map.util.UtilTestSuite;
+import com.risk.model.ModelTestSuite;
+import com.risk.validate.ValidateTestSuite;
+
+/**
+ * This is a master test suite of the project.
+ * @author Garvpreet Singh
+ * @version 1.0.1
+ */
+@RunWith(Suite.class)
+@SuiteClasses({UtilTestSuite.class, ModelTestSuite.class, ValidateTestSuite.class})
+public class MasterTestSuite {
+}
