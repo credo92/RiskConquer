622353955127cc8afefa1086fa837ff51ff1b5d6
Garvpreet Singh <garvs1401@gmail.com>
2017-11-07 14:26:21 -0500

    changed test cases

--- src/test/java/com/risk/model/PlayerModelTest.java
+++ src/test/java/com/risk/model/PlayerModelTest.java
@@ -1,6 +1,5 @@
 package com.risk.model;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,26 +9,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.risk.constant.CardType;
-import com.risk.constant.MapConstant;
 import com.risk.entity.Card;
 import com.risk.entity.Continent;
 import com.risk.entity.Map;
 import com.risk.entity.Player;
 import com.risk.entity.Territory;
 import com.risk.exception.InvalidGameMoveException;
-import com.risk.map.util.MapUtil;
-
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
 
 public class PlayerModelTest {
 	
@@ -42,18 +31,16 @@ public class PlayerModelTest {
 	static Player player;
 	@FXML
 	static TextArea textArea;
-	static ListView territoryListView;
+	
+	@FXML
+	static ListView<Territory> territoryListView;
 	
 	String continentName = "Asia";
 	String controlValue = "7";	
 	String territoryName1 = "India";
 	String territoryName2 = "China";	
 	
-	static List<Continent> listOfContinents;	
-	static List<Territory> listOfTerritories;
-	static List<Card> listOfCards;
 	static List<Player> players;
-
 	
 	static JFXPanel fxPanel;
 	
@@ -65,52 +52,56 @@ public class PlayerModelTest {
 		fxPanel = new JFXPanel();
 		gameModel = new GameModel();
 		playerModel = new PlayerModel();
-		continent = new Continent();
-		territory1 = new Territory();
-		territory2 = new Territory();
-		map = new Map();
-		player = new Player(1, "Sonu");	
-		textArea = new TextArea();
-		listOfContinents = new ArrayList<>();
-		listOfTerritories = new ArrayList<>();
-		listOfCards = new ArrayList<>();
+		textArea = new TextArea();		
 	}	
 	
 	/**
 	 * This method is invoked at the start of all the test methods.
 	 */
 	@Before
-	public void beforeTest() {
+	public void beforeTest() {	
+		territoryListView = new ListView<>();
+		map = new Map();
+		player = new Player(1, "Sonu");	
+		continent = new Continent();
+		territory1 = new Territory();
+		territory2 = new Territory();
+		
 		continent.setName(continentName);
 		continent.setValue(controlValue);
 		
 		territory1.setName(territoryName1);
 		territory1.setBelongToContinent(continent);
+		continent.getTerritories().add(territory1);
+		
 		territory2.setName(territoryName2);
 		territory2.setBelongToContinent(continent);		
+		continent.getTerritories().add(territory2);
 		
-		territory1.getAdjacentTerritories().add(territory1);
-		territory1.setAdjacentTerritories(territory2.getAdjacentTerritories());		
-
-		territory2.getAdjacentTerritories().add(territory2);
-		territory2.setAdjacentTerritories(territory1.getAdjacentTerritories());
+		territory1.getAdjacentTerritories().add(territory2);
+		territory2.getAdjacentTerritories().add(territory1);
 		
-		listOfContinents.add(continent);
-		map.setContinents(listOfContinents);
+		map.getContinents().add(continent);	
 		
-		player.setArmies(100);
+		territoryListView.setEditable(true);
+		territoryListView.getItems().add(territory1);
+		territoryListView.getItems().add(territory2);			
 		
-		listOfTerritories.add(territory1);
-		player.setAssignedTerritory(listOfTerritories);
+		playerModel.setPlayerPlaying(player);			
 	}
 	
 	/**
-	 * This method tests number of armies for 100 initial armies and 1 territory during each reinforcement phase.
+	 * This method tests number of armies for 35 initial armies and 1 territory during each reinforcement phase.
 	 */
 	@Test
 	public void calculateReinforcementArmiesCaseOne() {		
+		player.setArmies(35);
+		player.getAssignedTerritory().add(territory1);
+		territory1.setPlayer(player);
+		player.getAssignedTerritory().add(territory2);
+		territory2.setPlayer(player);
 		Player returnedPlayer = playerModel.calculateReinforcementArmies(map, player);
-		Assert.assertEquals(returnedPlayer.getArmies(), 110);
+		Assert.assertEquals(returnedPlayer.getArmies(), 45);
 	}
 	
 	/**
@@ -119,25 +110,50 @@ public class PlayerModelTest {
 	@Test
 	public void calculateReinforcementArmiesCaseTwo() {	
 		Continent newContinent = new Continent();
+		Territory terr = new Territory();
+		
 		newContinent.setName("Africa");
 		newContinent.setValue("5");
-		listOfContinents.add(newContinent);
-		map.setContinents(listOfContinents);
+		
+		terr.setName("New Territory");
+		terr.setBelongToContinent(newContinent);
+		
+		terr.getAdjacentTerritories().add(territory1);
+		territory1.getAdjacentTerritories().add(terr);
+		
+		newContinent.getTerritories().add(terr);
+		
+		map.getContinents().add(newContinent);
+		
+		player.setArmies(35);
+		player.getAssignedTerritory().add(territory1);
+		territory1.setPlayer(player);
+		player.getAssignedTerritory().add(territory2);
+		territory2.setPlayer(player);
+		
+		player.getAssignedTerritory().add(terr);
+		terr.setPlayer(player);
+		
 		Player returnedPlayer = playerModel.calculateReinforcementArmies(map, player);
-		Assert.assertEquals(returnedPlayer.getArmies(), 122);
-	}
+		Assert.assertEquals(returnedPlayer.getArmies(), 50);
+	}	
 	
-
+	/**
+	 * This method tests number of continents owned by a player.
+	 */
 	@Test
 	public void getContinentsOwnedByPlayer() {
-		List<Continent> continents = new ArrayList<>();
-		map.getContinents().get(0).setTerritories(listOfTerritories);
-		map.getContinents().get(0).getTerritories().get(0).setPlayer(player);
-		continents = playerModel.getContinentsOwnedByPlayer(map, player);
-		Assert.assertEquals("Asia", continents.get(0).getName());
-	}
+		List<Continent> returnedContinents = new ArrayList<>();
+		territory1.setPlayer(player);
+		territory2.setPlayer(player);
+		returnedContinents = playerModel.getContinentsOwnedByPlayer(map, player);
+		Assert.assertEquals("Asia", returnedContinents.get(0).getName());
+		Assert.assertEquals(1, returnedContinents.size());
+	}	
 	
-
+	/**
+	 * This method tests if a player has zero armies.
+	 */
 	@Test
 	public void checkIfPlayersArmiesExhausted() {
 		players = new ArrayList<>();
@@ -146,42 +162,54 @@ public class PlayerModelTest {
 		Assert.assertTrue(playerModel.checkIfPlayersArmiesExhausted(players));
 	}
 	
+	/**
+	 * This method tests if a player does not have zero armies.
+	 */
 	@Test
 	public void checkIfPlayersArmiesExhaustedFalseCase() {
 		players = new ArrayList<>();
 		players.add(new Player(0, "Player0"));
         players.get(0).setArmies(1);	
 		Assert.assertFalse(playerModel.checkIfPlayersArmiesExhausted(players));
-	}
+	}		
 	
-	
-	@Test
-	public void playerHasAValidAttackMoveFalseCase() {
-		territoryListView = new ListView<>();
-		territoryListView.getItems().add(listOfTerritories);
-		//not able to pass good type of list
-		//boolean result = playerModel.playerHasAValidAttackMove(territoryListView, textArea);
-	}
-	
+	/**
+	 * This method tests if a player have more than 1 army to attack.
+	 */
 	@Test
 	public void playerHasAValidAttackMove() {
-		territoryListView = new ListView<>();
-		territoryListView.getItems().add(listOfTerritories);
-		//not able to pass good type of list
-		//boolean result = playerModel.playerHasAValidAttackMove(territoryListView, textArea);
+		territory1.setArmies(5);
+		territory2.setArmies(3);
+		boolean actualResult = playerModel.playerHasAValidAttackMove(territoryListView, textArea);
+		Assert.assertTrue(actualResult);
 	}
 	
+	/**
+	 * This method tests if a player does not have more than 1 army to attack.
+	 */
+	@Test
+	public void playerHasAValidAttackMoveFalseCase() {	
+		territory1.setArmies(1);
+		territory2.setArmies(1);
+		boolean actualResult = playerModel.playerHasAValidAttackMove(territoryListView, textArea);
+		Assert.assertFalse(actualResult);
+	}
+	
+	/**
+	 * This method tests if a player has lost the territory.
+	 */
 	@Test
 	public void checkIfAnyPlayerLostTheGame() {
-		Player playerLost;
-		listOfTerritories = new ArrayList<>();
 		players = new ArrayList<>();
 		players.add(new Player(0, "Player0"));
-		players.get(0).setAssignedTerritory(listOfTerritories);
-		playerLost = playerModel.checkIfAnyPlayerLostTheGame(players);
-		Assert.assertEquals("Player0", players.get(0).getName());
+		players.get(0).setAssignedTerritory(new ArrayList<>());
+		Player playerLost = playerModel.checkIfAnyPlayerLostTheGame(players);
+		Assert.assertEquals(0, playerLost.getAssignedTerritory().size());
 	}
 	
+	/**
+	 * This method is used to test creation of players in the system.
+	 */
 	@Test
 	public void createPlayer() {
 		List<Player> playerTest = new ArrayList<>();
@@ -194,110 +222,39 @@ public class PlayerModelTest {
 	}
 	
 	/**
-	 * This method tests number of armies for 100 initial armies and 10 territory during each reinforcement phase.
-	 */
-	@Test
-	public void calculateReinforcementArmiesCaseThree() {		
-		Territory terr = new Territory();
-		terr.setName("Russia");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Canada");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Japan");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("UAE");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("UK");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("USA");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Mexico");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Pakistan");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Afghanistan");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Malaysia");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("Thailand");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		terr.setName("New Zealand");
-		terr.setBelongToContinent(continent);
-		listOfTerritories.add(terr);
-		
-		player.setAssignedTerritory(listOfTerritories);
-		
-		Player returnedPlayer = playerModel.calculateReinforcementArmies(map, player);
-		Assert.assertEquals(returnedPlayer.getArmies(), 145);
-	}
-	
-	/**
 	 * This method tests whether Fortification phase is valid by setting territories armies to 2  
 	 */
 	@Test
-	public void isFortificationPhaseValidTrue()
-	{  
-	 map.getContinents().get(0).setTerritories(listOfTerritories);
-	 map.getContinents().get(0).getTerritories().get(0).setPlayer(player);
-	 map.getContinents().get(0).getTerritories().get(0).setArmies(2);
-	 map.getContinents().get(0).getTerritories().get(0).getAdjacentTerritories().get(0).setPlayer(player);
-	 
-	 boolean isFortificationPhaseValid = playerModel.isFortificationPhaseValid(map, player);
-	 
-	 Assert.assertEquals(true, isFortificationPhaseValid);
+	public void isFortificationPhaseValidTrue(){
+		territory1.setPlayer(player);
+		territory1.setArmies(2);
+		territory2.setPlayer(player);	 
+		boolean isFortificationPhaseValid = playerModel.isFortificationPhaseValid(map, player);
+		Assert.assertEquals(true, isFortificationPhaseValid);
 	}
 	
 	/**
 	 * This method tests whether Fortification phase is valid by setting territories armies to 0  
 	 */
 	@Test
-	public void isFortificationPhaseValidFalse()
-	{  	 
-	 map.getContinents().get(0).setTerritories(listOfTerritories);
-	 map.getContinents().get(0).getTerritories().get(0).setPlayer(player);
-	 map.getContinents().get(0).getTerritories().get(0).setArmies(0);
-	 map.getContinents().get(0).getTerritories().get(0).getAdjacentTerritories().get(0).setPlayer(player);
-
-	 boolean isFortificationPhaseValid = playerModel.isFortificationPhaseValid(map, player);
-
-	 Assert.assertEquals(false, isFortificationPhaseValid);
+	public void isFortificationPhaseValidFalse(){  	 
+		territory1.setPlayer(player);
+		territory1.setArmies(0);
+		territory2.setPlayer(player);	 
+		boolean isFortificationPhaseValid = playerModel.isFortificationPhaseValid(map, player);
+		Assert.assertEquals(false, isFortificationPhaseValid);
 	}
 	
 	/**
 	 * This method checks whether attack move is valid or not by creating a new player
 	 */
 	@Test
-	public void isAValidAttackMoveTrue() throws InvalidGameMoveException
-	{   
+	public void isAValidAttackMoveTrue() throws InvalidGameMoveException{   
 		Player player2 = new Player(2, "Monu");
 		territory1.setPlayer(player);
 		territory2.setPlayer(player2);
-		territory1.setArmies(3);
-		
-		Assert.assertEquals(true,playerModel.isAValidAttackMove(territory1, territory2));
-		
+		territory1.setArmies(3);		
+		Assert.assertEquals(true,playerModel.isAValidAttackMove(territory1, territory2));		
 	}
 	
 	/**
@@ -305,15 +262,17 @@ public class PlayerModelTest {
 	 */
 	@Test
 	public void tradeCardsForArmy() {
+		List<Card> listOfCards = new ArrayList<>();
 		listOfCards.add(new Card(CardType.ARTILLERY));
 		listOfCards.add(new Card(CardType.CAVALRY));
 		listOfCards.add(new Card(CardType.INFANTRY));
-		playerModel.setPlayerPlaying(player);
 		Player returnedPlayer = playerModel.tradeCardsForArmy(listOfCards, 1, textArea);
-		Assert.assertEquals(105, returnedPlayer.getArmies());
+		Assert.assertEquals(5, returnedPlayer.getArmies());
 	}
 	
-	
+	/**
+	 * This method tests assignation of armies to a player.
+	 */
 	@Test
 	public void assignArmiesToPlayers() {
 		players = new ArrayList<>();
