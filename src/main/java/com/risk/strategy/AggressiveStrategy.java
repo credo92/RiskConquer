2ce7e73084afed31310cfee4cf0889404272c2bf
gurpreetsingh2410 <gurpreetsingh2410@gmail.com>
2017-11-18 00:57:58 -0500

    Aggressive strategy added

---
+++ src/main/java/com/risk/strategy/AggressiveStrategy.java
@@ -0,0 +1,63 @@
+package com.risk.strategy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+import com.risk.controller.DiceRollController;
+
+import com.risk.entity.Player;
+import com.risk.entity.Territory;
+import com.risk.exception.InvalidGameMoveException;
+import com.risk.model.PlayerGamePhase;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+
+public class AggressiveStrategy implements PlayerBehaviorStrategy{
+	
+
+	@Override
+	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory, TextArea gameConsole,
+			Player playerPlaying) {
+		List<Territory> list = new ArrayList<>(territoryList);
+		
+		Collections.sort(list, new Comparator<Territory>() {
+			@Override
+			public int compare(Territory o1, Territory o2) {
+				return Integer.valueOf(o2.getArmies()).compareTo(Integer.valueOf(o1.getArmies()));
+			}		
+		});
+		
+		list.get(0);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attackPhase(ListView<Territory> attackingTerritoryList, ListView<Territory> defendingTerritoryList,
+			PlayerGamePhase gamePhase) throws InvalidGameMoveException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean fortificationPhase(Territory selectedTerritory, Territory adjTerritory, TextArea gameConsole,
+			Player playerPlaying) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playerHasAValidAttackMove(ListView<Territory> territories, TextArea gameConsole) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+}
