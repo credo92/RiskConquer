caa9117cf507d7157ebd1d8a48825b956d1ce6c6
gurpreetsingh2410 <gurpreetsingh2410@gmail.com>
2017-11-18 00:57:58 -0500

    Aggressive class added

--- src/main/java/com/risk/strategy/AggressiveStrategy.java
+++ src/main/java/com/risk/strategy/AggressiveStrategy.java
@@ -1,24 +1,46 @@
 package com.risk.strategy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
 import com.risk.controller.DiceRollController;
+
 import com.risk.entity.Player;
 import com.risk.entity.Territory;
 import com.risk.exception.InvalidGameMoveException;
+import com.risk.model.PlayerGamePhase;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
+
 public class AggressiveStrategy implements PlayerBehaviorStrategy{
+	
 
 	@Override
-	public void reinforcementPhase(Territory territory, TextArea gameConsole, Player playerPlaying) {
+	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory, TextArea gameConsole,
+			Player playerPlaying) {
+		List<Territory> list = new ArrayList<>(territoryList);
+		
+		Collections.sort(list, new Comparator<Territory>() {
+			@Override
+			public int compare(Territory o1, Territory o2) {
+				return Integer.valueOf(o2.getArmies()).compareTo(Integer.valueOf(o1.getArmies()));
+			}		
+		});
+		
+		list.get(0);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void attackPhase(Territory attackingTerritory, Territory defendingTerritory,
-			DiceRollController diceController) throws InvalidGameMoveException {
+	public void attackPhase(ListView<Territory> attackingTerritoryList, ListView<Territory> defendingTerritoryList,
+			PlayerGamePhase gamePhase) throws InvalidGameMoveException {
 		// TODO Auto-generated method stub
 		
 	}
@@ -35,5 +57,7 @@ public class AggressiveStrategy implements PlayerBehaviorStrategy{
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+
 
 }
