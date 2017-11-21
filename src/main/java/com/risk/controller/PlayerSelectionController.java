71291a31f61366529422d4c786ecd31cef92f366
gurpreetsingh2410 <gurpreetsingh2410@gmail.com>
2017-11-21 01:35:19 -0500

    Player View Done

--- src/main/java/com/risk/controller/PlayerSelectionController.java
+++ src/main/java/com/risk/controller/PlayerSelectionController.java
@@ -3,11 +3,19 @@ package com.risk.controller;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import com.risk.model.PlayerGamePhase;
+import com.risk.strategy.AggressiveStrategy;
+import com.risk.strategy.BenevolentStrategy;
+import com.risk.strategy.CheaterStrategy;
+import com.risk.strategy.HumanStrategy;
+import com.risk.strategy.PlayerBehaviorStrategy;
+import com.risk.strategy.RandomStrategy;
 import com.risk.constant.PlayerType;
 import com.risk.entity.Player;
 import com.risk.map.util.GameUtil;
+import com.risk.map.util.MapUtil;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -25,117 +33,146 @@ import javafx.scene.layout.VBox;
 
 /**
  * Player Selection controller.
+ * 
  * @author Gurpreet Singh
  * @version 1.0.0
  */
 
-public class PlayerSelectionController implements Initializable{
+public class PlayerSelectionController implements Initializable {
 
 	/**
 	 * The @vBoxPane .
 	 */
 	@FXML
 	private VBox vBoxPane;
-	
+
 	/**
 	 * The @exitButton.
 	 */
 	@FXML
 	private Button okButton;
-	
+
 	/**
 	 * The @cancelButton.
 	 */
 	@FXML
 	private Button cancelButton;
-	
-	private Player player;
 
-		
-	
 	private List<Player> playerList;
-	
+
 	public PlayerSelectionController(List<Player> playerList) {
 		this.playerList = playerList;
 	}
-	
-	
-	
-	
+
 	/*
 	 * (non-Javadoc) Card controller initializer, loading currentPlayer and current
 	 * 
 	 * @see javafx.fxml.Initializable#initialize(java.net.URL,
 	 * java.util.ResourceBundle)
 	 */
-	public void initialize(URL location, ResourceBundle resources) {		
+	public void initialize(URL location, ResourceBundle resources) {
 		loadAllPlayers();
-		
+
 	}
-	
+
 	public HBox getElementsWindow(int playerId) {
 		ChoiceBox<PlayerType> playerType = new ChoiceBox<>();
 		playerType.getItems().addAll(PlayerType.values());
-		
+		playerType.getSelectionModel().selectFirst();
+
 		TextField textField = new TextField();
-		
+
 		HBox hBox = new HBox();
 		hBox.setSpacing(10);
-		
+
 		Label playerIdShow = new Label();
-		playerIdShow.setText(String.valueOf("Player "+ playerId));
-		
+		playerIdShow.setText(String.valueOf(playerId));
+
 		hBox.getChildren().addAll(playerIdShow, textField, playerType);
-		
+
 		return hBox;
 	}
-	
+
 	/**
 	 * Load All players.
 	 */
 	public void loadAllPlayers() {
 		vBoxPane.setSpacing(10);
-		
-		for(int i = 0; i < playerList.size(); i++) {
-			HBox getBox = getElementsWindow(Integer.valueOf(playerList.get(i).getId()));			
+
+		for (int i = 0; i < playerList.size(); i++) {
+			HBox getBox = getElementsWindow(Integer.valueOf(playerList.get(i).getId()));
 			vBoxPane.getChildren().addAll(getBox);
 		}
-		
+
 	}
-	
+
+	public PlayerBehaviorStrategy getStrategyObject(String strategyType) {
+		PlayerBehaviorStrategy strategy = null;
+		if (strategyType.equals("Human")) {
+			strategy = new HumanStrategy();
+		} else if (strategyType.equals("AGGRESSIVE")) {
+			strategy = new AggressiveStrategy();
+
+		} else if (strategyType.equals("BENEVOLENT")) {
+			strategy = new BenevolentStrategy();
+
+		} else if (strategyType.equals("RANDOM")) {
+			strategy = new RandomStrategy();
+
+		} else if (strategyType.equals("CHEATER")) {
+			strategy = new CheaterStrategy();
+
+		}
+		return strategy;
+	}
+
 	public boolean validateTextFields(ObservableList<Node> hBoxList) {
-		for(Node n : hBoxList) {
-			HBox box = (HBox)n;
+		for (Node n : hBoxList) {
+			HBox box = (HBox) n;
 			for (Node node : box.getChildren()) {
-				if(node instanceof TextField) {
-					if((TextField)node != null) {
-						return true;
+				if (node instanceof TextField) {
+					if ((TextField) node == null || ((TextField) node).getText().trim().isEmpty()) {
+						return false;
 					}
 				}
 			}
 		}
-		return false;
+		return true;
 	}
-	
+
 	/**
-	 *save players
+	 * save players
 	 * 
 	 * @param event
 	 *            action event
 	 */
 	public void savePlayerType(ActionEvent event) {
+		boolean flag = false;
 		ObservableList<Node> hBoxList = vBoxPane.getChildren();
-		if(validateTextFields(hBoxList)) {
-			for(Node n : hBoxList) {
-				HBox box = (HBox)n;
-				for (Node node : box.getChildren()) {
-					if(node instanceof TextField) {
-						player.setName(((TextField)node).getText());
-					}
+		if (validateTextFields(hBoxList)) {
+			for (Node n : hBoxList) {
+				HBox box = (HBox) n;
+				ObservableList<Node> node = box.getChildren();
+				List<Player> players = playerList.stream()
+						.filter(p -> p.getId() == Integer.parseInt(((Label) node.get(0)).getText()))
+						.collect(Collectors.toList());
+				if (node.get(1) instanceof TextField) {
+					players.get(0).setName(((TextField) node.get(1)).getText());
+					flag = true;
 				}
+				if (node.get(2) instanceof ChoiceBox<?>) {
+					PlayerType selectedPlayerType = (PlayerType) ((ChoiceBox<?>) node.get(2)).getSelectionModel().getSelectedItem();
+					PlayerBehaviorStrategy strategy = getStrategyObject(selectedPlayerType.toString());
+					players.get(0).setStrategy(strategy);
+				}
+
 			}
 		}
+		if (flag) {
+			GameUtil.closeScreen(okButton);
+		} else {
+			MapUtil.infoBox("Please fill all the details", "Message", "");
+		}
 
-		
 	}
 }
---
+++ src/main/java/com/risk/strategy/CheaterStrategy.java
@@ -0,0 +1,41 @@
+package com.risk.strategy;
+
+import com.risk.entity.Player;
+import com.risk.entity.Territory;
+import com.risk.exception.InvalidGameMoveException;
+import com.risk.model.PlayerGamePhase;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class CheaterStrategy implements PlayerBehaviorStrategy {
+
+	@Override
+	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory, TextArea gameConsole,
+			Player playerPlaying) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attackPhase(ListView<Territory> attackingTerritoryList, ListView<Territory> defendingTerritoryList,
+			PlayerGamePhase gamePhase) throws InvalidGameMoveException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean fortificationPhase(Territory selectedTerritory, Territory adjTerritory, TextArea gameConsole,
+			Player playerPlaying) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean playerHasAValidAttackMove(ListView<Territory> territories, TextArea gameConsole) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
