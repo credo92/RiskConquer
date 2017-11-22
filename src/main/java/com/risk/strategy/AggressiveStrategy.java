420eaf84ef2c64d5aaf2a32c25b08ec76e4d0e3f
gurpreetsingh2410 <gurpreetsingh2410@gmail.com>
2017-11-22 01:25:17 -0500

    reiforcement phase added

--- src/main/java/com/risk/strategy/AggressiveStrategy.java
+++ src/main/java/com/risk/strategy/AggressiveStrategy.java
@@ -3,7 +3,9 @@ package com.risk.strategy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.risk.entity.Player;
 import com.risk.entity.Territory;
@@ -14,31 +16,61 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
+public class AggressiveStrategy implements PlayerBehaviorStrategy {
 
-public class AggressiveStrategy implements PlayerBehaviorStrategy{
-	
+	public boolean getTerritoryOwnedByAnotherPlayer(Territory territory) {
+		List<Territory> listWithValidAdjacentTerriroty = territory.getAdjacentTerritories().stream()
+				.filter(t -> !(territory.getPlayer().equals(t.getPlayer()))).collect(Collectors.toList());
+		if (listWithValidAdjacentTerriroty.size() > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public Territory validTerritory(Iterator<Territory> listIterator, List<Territory> visitedTerritory) {
+		if (listIterator.hasNext()) {
+			Territory t = listIterator.next();
+			if (!visitedTerritory.contains(t)) {
+				if (getTerritoryOwnedByAnotherPlayer(t)) {
+					return t;
+				} else {
+					validTerritory(listIterator, visitedTerritory);
+				}
+			} else {
+				validTerritory(listIterator, visitedTerritory);
+			}
+		}
+		return null;
+	}
 
 	@Override
 	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory, TextArea gameConsole,
 			Player playerPlaying) {
+
 		List<Territory> list = new ArrayList<>(territoryList);
-		
+		List<Territory> visitedTerritory = new ArrayList<>();
+
 		Collections.sort(list, new Comparator<Territory>() {
 			@Override
 			public int compare(Territory o1, Territory o2) {
 				return Integer.valueOf(o2.getArmies()).compareTo(Integer.valueOf(o1.getArmies()));
-			}		
+			}
 		});
+		Iterator<Territory> listIterator = list.iterator();
+		Territory t = validTerritory(listIterator, visitedTerritory);
 		
-		list.get(0);
-		// TODO Auto-generated method stub
-		
+		if(t != null) {
+			t.setArmies(playerPlaying.getArmies());
+		}else {
+			list.get(0).setArmies(playerPlaying.getArmies());
+		}
+
 	}
 
 	public void attackPhase(ListView<Territory> attackingTerritoryList, ListView<Territory> defendingTerritoryList,
 			PlayerGamePhase gamePhase) throws InvalidGameMoveException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public boolean fortificationPhase(ListView<Territory> selectedTerritory, ListView<Territory> adjTerritory,
@@ -51,8 +83,5 @@ public class AggressiveStrategy implements PlayerBehaviorStrategy{
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-
-
 
 }
