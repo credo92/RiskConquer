726c9b65923e8d8da463ab8be042848576b7247b
Garvpreet Singh <garvs1401@gmail.com>
2017-11-23 01:44:37 -0500

    reinforcement for Cheater stragy added

--- src/main/java/com/risk/strategy/CheaterStrategy.java
+++ src/main/java/com/risk/strategy/CheaterStrategy.java
@@ -3,6 +3,7 @@ package com.risk.strategy;
 import com.risk.entity.Player;
 import com.risk.entity.Territory;
 import com.risk.exception.InvalidGameMoveException;
+import com.risk.map.util.MapUtil;
 import com.risk.model.PlayerGamePhase;
 
 import javafx.collections.ObservableList;
@@ -13,15 +14,19 @@ public class CheaterStrategy implements PlayerBehaviorStrategy {
 
 	@Override
 	public void reinforcementPhase(ObservableList<Territory> territoryList, Territory territory, TextArea gameConsole,
-			Player playerPlaying) {
-		// TODO Auto-generated method stub
-		
+			Player playerPlaying) {		
+		for (Territory terr : territoryList) {
+			terr.setArmies(terr.getArmies() * 2);
+			MapUtil.appendTextToGameConsole("Armies have been doubled on territory " + terr.getName() + "\n",
+					gameConsole);
+		}		
+		playerPlaying.setArmies(0);
 	}
 
 	@Override
 	public void attackPhase(ListView<Territory> attackingTerritoryList, ListView<Territory> defendingTerritoryList,
 			PlayerGamePhase gamePhase, TextArea gameConsole) throws InvalidGameMoveException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
