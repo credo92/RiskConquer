cea01831a07b34c7fe303d145deab48038e1cebb
Garvpreet Singh <garvs1401@gmail.com>
2017-11-23 20:45:49 -0500

    cheater strategy files added

--- src/main/java/com/risk/strategy/CheaterStrategy.java
+++ src/main/java/com/risk/strategy/CheaterStrategy.java
@@ -1,9 +1,16 @@
 package com.risk.strategy;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.risk.controller.DiceRollController;
 import com.risk.entity.Player;
 import com.risk.entity.Territory;
 import com.risk.exception.InvalidGameMoveException;
 import com.risk.map.util.MapUtil;
+import com.risk.model.DiceModel;
 import com.risk.model.PlayerGamePhase;
 
 import javafx.collections.ObservableList;
@@ -27,20 +34,61 @@ public class CheaterStrategy implements PlayerBehaviorStrategy {
 	public void attackPhase(ListView<Territory> attackingTerritoryList, ListView<Territory> defendingTerritoryList,
 			PlayerGamePhase gamePhase, TextArea gameConsole) throws InvalidGameMoveException {
 		
-		
-	}
+		List<Territory> territoryWon = new ArrayList<Territory>();
+		ObservableList<Territory> attackTerList = attackingTerritoryList.getItems();
+		Iterator<Territory> terrIterator = attackTerList.iterator();
+		while (terrIterator.hasNext()) {
+			Territory attackingTerritory = terrIterator.next();
+			List<Territory> defendingTerritories = getDefendingTerritory(attackingTerritory);
+			
+			for (Territory defendingTerr : defendingTerritories) {
+				defendingTerr.setPlayer(attackingTerritory.getPlayer());
+				defendingTerr.setArmies(1);
+				attackingTerritory.setArmies(attackingTerritory.getArmies()-1);
+				attackingTerritory.getPlayer().getAssignedTerritory().add(defendingTerr);
+				territoryWon.add(defendingTerr);
+				MapUtil.appendTextToGameConsole(defendingTerr.getName() + " has been conquered by " + attackingTerritory.getPlayer().getName() + "\n",
+						gameConsole);
+			}
+		}
+		attackingTerritoryList.getItems().addAll(territoryWon);
+	}		
+	
+	/*private void attack(Territory attacking, Territory defending, PlayerGamePhase gamePhase, TextArea gameConsole) {
+		MapUtil.appendTextToGameConsole("Attack Cheater started!++++++++++++++++++++++++++++++++++++++++++ \n", gameConsole);
+		DiceModel diceModel = new DiceModel(attacking, defending);
+		diceModel.addObserver(gamePhase);
 
+		DiceRollController diceController = new DiceRollController(diceModel, this, gameConsole);
+		diceController.cheaterAttack();
+	}*/
+	
+	public boolean fortificationPhase(ListView<Territory> selectedTerritoryList, ListView<Territory> adjTerritoryList,
+			TextArea gameConsole, Player playerPlaying) {
+		
+		boolean isFortificationDone = false;
+		
+		ObservableList<Territory> selectedTerrList = selectedTerritoryList.getItems();		
+		Iterator<Territory> iterateTerritory = selectedTerrList.iterator();
+		
+		while (iterateTerritory.hasNext()) {
+			Territory fortifyingTerritory = iterateTerritory.next();
+			List<Territory> particularFortifyingTerritory = fortifyingTerritory.getAdjacentTerritories().stream()
+					.filter(t -> (fortifyingTerritory.getPlayer() != t.getPlayer()))
+					.collect(Collectors.toList());
+			
+			for (Territory t : particularFortifyingTerritory) {
+				t.setArmies(t.getArmies()*2);
+				MapUtil.appendTextToGameConsole("Armies have been doubled on territory " + t.getName() + "\n", gameConsole);
+				isFortificationDone = true;
+			}
+		}
+		return isFortificationDone;
+	}
 
 	@Override
 	public boolean playerHasAValidAttackMove(ListView<Territory> territories, TextArea gameConsole) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean fortificationPhase(ListView<Territory> selectedTerritory, ListView<Territory> adjTerritory,
-			TextArea gameConsole, Player playerPlaying) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
